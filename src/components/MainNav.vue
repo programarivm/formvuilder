bd1245b13c8e1f452ddf0d77555861bd4d72ceea
programarivm <info@programarivm.com>
2021-03-30 16:14:56 +0200

    Added main navigation bar

---
+++ src/components/MainNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <md-toolbar>
+      <h3 class="md-title" style="flex: 1">Form Builder</h3>
+      <md-button>About</md-button>
+      <md-button class="md-primary">Build</md-button>
+    </md-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainNav'
+}
+</script>
