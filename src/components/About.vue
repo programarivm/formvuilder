73e5d46970fc9ab1d88edbd8c6b9884316857701
programarivm <info@programarivm.com>
2021-03-31 14:44:27 +0200

    About content is added

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,44 @@
 <template>
   <div>
-    <h1>About</h1>
-    <p>TODO</p>
+    <md-content>
+      <h1>It's a piece of cake!</h1>
+      <h2>Build HTML forms without writing any code at all.</h2>
+      <h2>Just add, drag and drop, gracious, graceful cards.</h2>
+    </md-content>
+    <md-card class="md-layout-item md-small-size-95 md-size-20">
+      <md-card-content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </md-card-content>
+    </md-card>
+    <md-card class="md-layout-item md-small-size-95 md-size-20">
+      <md-card-content>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </md-card-content>
+    </md-card>
+    <md-card class="md-layout-item md-small-size-95 md-size-20">
+      <md-card-content>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      </md-card-content>
+    </md-card>
+    <md-card class="md-layout-item md-small-size-95 md-size-20">
+      <md-card-content>
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </md-card-content>
+    </md-card>
   </div>
 </template>
+
+<style scoped>
+  .md-content {
+    margin: 3rem 0 2rem 0;
+    background-color: transparent;
+  }
+  .md-card {
+    margin: 1rem 0.5rem;
+    display: inline-block;
+    vertical-align: top;
+  }
+</style>
 
 <script>
 export default {
