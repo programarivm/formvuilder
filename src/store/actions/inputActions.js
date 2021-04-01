d8e21ee9042e26d443f1decfd1e5e404cdd564d2
programarivm <info@programarivm.com>
2021-04-01 12:00:32 +0200

    Added Vuex constants and actions to write logic for the HTML inputs

---
+++ src/store/actions/inputActions.js
@@ -0,0 +1,25 @@
+import inputActionTypes from 'constants/inputActionTypes'
+
+export const add = ({commit}, payload) => {
+  commit(inputActionTypes.ADD, payload)
+}
+
+export const cancel = () => {
+  commit(inputActionTypes.CLOSE_ADD_DIALOG)
+}
+
+export const click = () => {
+  commit(inputActionTypes.OPEN_ADD_DIALOG)
+}
+
+export const del = ({commit}, payload) => {
+  commit(inputActionTypes.DELETE, payload)
+}
+
+export const set = ({commit}, payload) => {
+  commit(inputActionTypes.SET, payload)
+}
+
+export const add = ({commit}, payload) => {
+  commit(inputActionTypes.ADD, payload)
+}
